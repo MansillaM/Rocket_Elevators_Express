484217b283aa5083ae8ea64666c40b29ecdac2df
MansillaM <matmansilla1992@gmail.com>
2023-02-12 15:36:28 -0500

    email-list done

---
+++ email-list.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express()
+const port = process.env.PORT
+
+//import all agents from top-agents.js
+var topAgents = require('./top-agents')
+
+//js to get email from every agents
+let text = "";
+for (let a = 0; a < topAgents.length; a++) {
+    text += topAgents[a].email + ", ";
+}
+
+app.get('/email-list', (req, res) => {
+    res.send(text)
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
--- hello-world.js
+++ hello-world.js
@@ -4,7 +4,6 @@ dotenv.config();
 const app = express()
 const port = process.env.PORT
 
-
 app.get('/hello', (req, res) => {
     res.send('Hello World, from CodeBoxx');
 });
