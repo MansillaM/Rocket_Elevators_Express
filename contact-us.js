223a491bae78a2c5f92e7f29f766dc8cfe301a15
MansillaM <matmansilla1992@gmail.com>
2023-02-16 10:02:21 -0500

    post contact finish

---
+++ contact-us.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express()
+const port = process.env.PORT;
+app.use(express.json());
+
+app.post('/contact-us', (req, res) => {
+    const firstName = req.body.first_name;
+    const lastName = req.body.last_name;
+    const message = req.body.message;
+
+    const responseMessage = `Please contact ${firstName} ${lastName}`;
+
+    console.log(responseMessage);
+    res.send(responseMessage);
+});
+
+app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
